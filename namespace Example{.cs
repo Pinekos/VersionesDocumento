c95dcd5339182480f712de70f63e608db3edcda3
Pilar <113586357+Pinekos@users.noreply.github.com>
2022-09-29 09:33:03 +0200

    Add files via upload

--- namespace Example{.cs
+++ namespace Example{.cs
@@ -2,7 +2,7 @@ namespace Example{
     class Product{
     int a = 1;
     int b = 2;
-    int result = a*b;
-    Console.WriteLine(result);
+    int result = b*a;
+    Console.WriteLine(result+a);
     }
 }
